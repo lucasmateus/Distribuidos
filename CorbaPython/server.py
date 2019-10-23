6ab79d4ffa12aa273bfd93ee8cfc8de10878a094
Lucas Mateus <31706781+lucasmateus@users.noreply.github.com>
2019-10-23 08:28:42 -0300

    Update server.py

--- CorbaPython/server.py
+++ CorbaPython/server.py
@@ -1,23 +1,23 @@
 import sys, os
 import CORBA, Fortune, Fortune__POA
 
-FORTUNE_PATH = "/home/lucasmateus/novapasta2/fortune"
-
 class CookieServer_i(Fortune__POA.CookieServer):
     def get_cookie(self):
-        pipe   = os.popen(FORTUNE_PATH)
-        cookie = pipe.read()
-        if pipe.close():
-            cookie = "Ola Mundo Python Corba\n"
-        return cookie
-
+            msg = "Ola Mundo Python Corba\n"
+        return msg
+#inicia o orb do corba
 orb = CORBA.ORB_init(sys.argv)
+#POA raiz servidor -> cliente
 poa = orb.resolve_initial_references("RootPOA")
 
+#instancia o server
 servant = CookieServer_i()
+#ativa o objeto do serve com a poa raiz
 poa.activate_object(servant)
 
+#print na tela
 print orb.object_to_string(servant._this())
 
+#Referencia a POA ativa
 poa._get_the_POAManager().activate()
 orb.run()
